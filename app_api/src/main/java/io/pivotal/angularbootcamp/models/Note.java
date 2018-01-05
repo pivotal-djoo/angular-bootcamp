a1f4f6471cf4fa30dc9eb69f79d09fff32876d33
David Joo and Ka Hin Ng <pair+djoo+kang@pivotal.io>
2018-01-05 15:01:07 -0500

    Add api unit tests

--- app_api/src/main/java/io/pivotal/angularbootcamp/models/Note.java
+++ app_api/src/main/java/io/pivotal/angularbootcamp/models/Note.java
@@ -1,14 +1,19 @@
 package io.pivotal.angularbootcamp.models;
 
+import lombok.Builder;
+import lombok.Value;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Value
+@Builder
 @Entity
 public class Note {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    public Integer id;
-    public String content;
+    Integer id;
+    String content;
 }
---
+++ app_api/src/test/java/io/pivotal/angularbootcamp/NotesControllerTest.java
@@ -0,0 +1,86 @@
+package io.pivotal.angularbootcamp;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.pivotal.angularbootcamp.models.Note;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.contains;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+public class NotesControllerTest {
+
+    @Mock
+    NotesRepository notesRepository;
+
+    @Captor
+    ArgumentCaptor<Note> argCaptor;
+
+    @InjectMocks
+    NotesController subject;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+            .standaloneSetup(subject)
+            .build();
+    }
+
+    @Test
+    public void addNote() throws Exception {
+        mvc.perform(post("/add")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(asJsonString(Note.builder().content("new note!").build())))
+            .andExpect(status().isCreated());
+
+        verify(notesRepository, times(1)).save(argCaptor.capture());
+        assertThat(argCaptor.getValue().getContent()).isEqualTo("new note!");
+    }
+
+    @Test
+    public void getAllNotes() throws Exception {
+        List<Note> notes = asList(
+            Note.builder().content("first note").build(),
+            Note.builder().content("second note").build()
+        );
+
+        when(notesRepository.findAll()).thenReturn(notes);
+
+        mvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(content().json(asJsonString(notes)));
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
