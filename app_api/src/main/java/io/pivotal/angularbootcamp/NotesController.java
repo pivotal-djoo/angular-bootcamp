b20ef598bf718c466b2b3b419b0b48d0981f7204
David Joo and Ka Hin Ng <pair+djoo+kang@pivotal.io>
2018-01-08 11:04:01 -0500

    Set NotesController to accept all CORS

--- app_api/src/main/java/io/pivotal/angularbootcamp/NotesController.java
+++ app_api/src/main/java/io/pivotal/angularbootcamp/NotesController.java
@@ -4,13 +4,11 @@ import io.pivotal.angularbootcamp.models.Note;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class NotesController {
 
